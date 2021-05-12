2cf386074917e391ae7f2f2d9b99690e29c204ed
KieranClem <76912798+KieranClem@users.noreply.github.com>
2021-05-12 12:12:45 +0100

    Level design and small adjustments
    Level expanded upon and made larger, made small adjustment to fights to show exact HP

--- Assets/Scripts/BattleHud.cs
+++ Assets/Scripts/BattleHud.cs
@@ -8,6 +8,8 @@ public class BattleHud : MonoBehaviour
     public Text nameText;
     public Text levelText;
     public Slider hpSlider;
+    public Text HP;
+    private int MaxHP;
 
     public void SetHud(Unit unit)
     {
@@ -15,10 +17,16 @@ public class BattleHud : MonoBehaviour
         levelText.text = "Level: " + unit.unitLevel;
         hpSlider.maxValue = unit.maxHP;
         hpSlider.value = unit.currentHP;
+        MaxHP = unit.maxHP;
+        HP.text = "HP: " + unit.currentHP + "/" + unit.maxHP;
     }
 
     public void SetHP(int hp)
     {
         hpSlider.value = hp;
+        if(hp < 0)
+            HP.text = "HP: " + 0 + "/" + MaxHP;
+        else
+            HP.text = "HP: " + hp + "/" + MaxHP;
     }
 }
